b7eafe469fe27fa82a3168b5696251994c55f529
KC <kasun@emerico.biz>
2020-02-07 18:08:49 +0800

    Modified

--- FaceUploader/Controllers/FaceMasterController.cs
+++ FaceUploader/Controllers/FaceMasterController.cs
@@ -27,14 +27,74 @@ namespace FaceUploader.Controllers
             return View(await _context.FaceMaster.ToListAsync());
         }
 
-        public async Task<IActionResult> UploadP0ToDirectory(IFormFile file)
+        public async Task<IActionResult> UploadP0ToDirectory(List<IFormFile> files)
+        {
+            if (files.Count > 0)
+            {
+
+                foreach (var file in files)
+                {
+
+                    string tempFileName = file.FileName;
+                    var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\P0", file.FileName);
+
+                    int count = 1;
+
+                    string fileNameOnly = Path.GetFileNameWithoutExtension(fullPath);
+                    string extension = Path.GetExtension(fullPath);
+                    string path = Path.GetDirectoryName(fullPath);
+                    string newFullPath = fullPath;
+
+                    if (!string.Equals(extension, ".jpg", StringComparison.OrdinalIgnoreCase)
+                        && !string.Equals(extension, ".png", StringComparison.OrdinalIgnoreCase)
+                        && !string.Equals(extension, ".gif", StringComparison.OrdinalIgnoreCase)
+                        && !string.Equals(extension, ".jpeg", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return View("Create");
+                    }
+                    else
+                    {
+                        while (System.IO.File.Exists(newFullPath))
+                        {
+                            tempFileName = string.Format("{0}({1})", fileNameOnly, count++);
+                            newFullPath = Path.Combine(path, tempFileName + extension);
+                            tempFileName = tempFileName + extension;
+                        }
+
+                        using (var stream = new FileStream(newFullPath, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+                        return RedirectToAction("DisplayP0", "FaceMaster", new { imageName = tempFileName });
+                    }
+
+                }
+            }
+            return View("Create");
+
+        }
+
+        public ActionResult DisplayP0(string imageName)
+        {
+
+            var directory = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\P0"));
+            var myFile = directory.GetFiles(imageName).FirstOrDefault();
+
+
+            ViewBag.fileNameP0 = myFile.Name;
+            ViewBag.pathP0 = "/images/P0/" + myFile.Name;
+
+            return View("Create");
+        }
+
+        public async Task<IActionResult> UploadP45ToDirectory(IFormFile file)
         {
 
 
             if (file.Length > 0)
             {
                 string tempFileName = file.FileName;
-                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\P0", file.FileName);
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\P45", file.FileName);
 
                 int count = 1;
 
@@ -63,7 +123,7 @@ namespace FaceUploader.Controllers
                     {
                         await file.CopyToAsync(stream);
                     }
-                    return RedirectToAction("DisplayP0", "FaceMaster", new { imageName = tempFileName });
+                    return RedirectToAction("DisplayP45", "FaceMaster", new { imageName = tempFileName });
                 }
 
             }
@@ -72,15 +132,73 @@ namespace FaceUploader.Controllers
 
         }
 
-        public ActionResult DisplayP0(string imageName)
+        public ActionResult DisplayP45(string imageName)
         {
 
-            var directory = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\P0"));
+            var directory = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\P45"));
             var myFile = directory.GetFiles(imageName).FirstOrDefault();
 
 
-            ViewBag.fileName = myFile.Name;
-            ViewBag.path = "/images/P0/" + myFile.Name;
+            ViewBag.fileNameP45 = myFile.Name;
+            ViewBag.pathP45 = "/images/P45/" + myFile.Name;
+
+            return View("Create");
+        }
+
+        public async Task<IActionResult> UploadP90ToDirectory(IFormFile file)
+        {
+
+
+            if (file.Length > 0)
+            {
+                string tempFileName = file.FileName;
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\P90", file.FileName);
+
+                int count = 1;
+
+                string fileNameOnly = Path.GetFileNameWithoutExtension(fullPath);
+                string extension = Path.GetExtension(fullPath);
+                string path = Path.GetDirectoryName(fullPath);
+                string newFullPath = fullPath;
+
+                if (!string.Equals(extension, ".jpg", StringComparison.OrdinalIgnoreCase)
+                    && !string.Equals(extension, ".png", StringComparison.OrdinalIgnoreCase)
+                    && !string.Equals(extension, ".gif", StringComparison.OrdinalIgnoreCase)
+                    && !string.Equals(extension, ".jpeg", StringComparison.OrdinalIgnoreCase))
+                {
+                    return View("Create");
+                }
+                else
+                {
+                    while (System.IO.File.Exists(newFullPath))
+                    {
+                        tempFileName = string.Format("{0}({1})", fileNameOnly, count++);
+                        newFullPath = Path.Combine(path, tempFileName + extension);
+                        tempFileName = tempFileName + extension;
+                    }
+
+                    using (var stream = new FileStream(newFullPath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                    return RedirectToAction("DisplayP90", "FaceMaster", new { imageName = tempFileName });
+                }
+
+            }
+
+            return View("Create");
+
+        }
+
+        public ActionResult DisplayP90(string imageName)
+        {
+
+            var directory = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images\\P90"));
+            var myFile = directory.GetFiles(imageName).FirstOrDefault();
+
+
+            ViewBag.fileNameP90 = myFile.Name;
+            ViewBag.pathP90 = "/images/P90/" + myFile.Name;
 
             return View("Create");
         }
